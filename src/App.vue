4011d5a6d27f78aff80f7f057490b0788ab9d36d
xx <1329629708@qq.com>
2022-10-24 20:11:28 +0800

    提交supertofu所有文件

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,16 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <router-view></router-view>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import './assets/css/reset3.0.css'
+import './assets/css/swiper.min.css'
+import './assets/js/swiper.min.js'
+import './api/request.js'
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
-  }
 }
 </script>
 
-<style lang="less">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+<style></style>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,17 @@
 import Vue from 'vue'
 import App from './App.vue'
+import router from './router'
+import axios from 'axios'
+import store from './store'
 
 Vue.config.productionTip = false
 
+axios.defaults.baseURL = 'http://127.0.0.1'
+// 把axios挂载到Vue.prototype上，供每个.vue组件的实例直接使用
+Vue.prototype.$http = axios
+
 new Vue({
+  router,
+  store,
   render: h => h(App),
 }).$mount('#app')
